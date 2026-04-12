e886fdc825f252f20589c9c53c9da0d4897f6754
Iouri Kostine <kostine@gmail.com>
2026-04-11 19:35:24 -0500

    formula: update to v0.2.2

--- flightplan.rb
+++ flightplan.rb
@@ -6,31 +6,31 @@ require_relative "lib/custom_download_strategy"
 class Flightplan < Formula
   desc "Local-first issue tracker for AI agent workflows"
   homepage "https://github.com/turn-radius/flightplan"
-  version "0.2.1"
+  version "0.2.2"
 
   on_macos do
     on_intel do
-      url "https://github.com/turn-radius/flightplan/releases/download/v0.2.1/flightplan_darwin_amd64.tar.gz",
+      url "https://github.com/turn-radius/flightplan/releases/download/v0.2.2/flightplan_darwin_amd64.tar.gz",
           using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "20e931181a44441c5afcd5640ff4bc7187cb445911a1bee59726220ced67801b"
+      sha256 "1b7e3868e079ac212ff4bcefad33f1a87cbebb207c1f2096498ed5c25d0be88e"
     end
     on_arm do
-      url "https://github.com/turn-radius/flightplan/releases/download/v0.2.1/flightplan_darwin_arm64.tar.gz",
+      url "https://github.com/turn-radius/flightplan/releases/download/v0.2.2/flightplan_darwin_arm64.tar.gz",
           using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "ae4be8845a4e0eec98ab2996e7ee996030e5df0f677d6c03a2f335a5e82366d2"
+      sha256 "fbba40a17edb61c5eb9786447de19dd8b35b513cd52e5c1eb4513cb708e0967e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/turn-radius/flightplan/releases/download/v0.2.1/flightplan_linux_amd64.tar.gz",
+      url "https://github.com/turn-radius/flightplan/releases/download/v0.2.2/flightplan_linux_amd64.tar.gz",
           using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "30732ef5f015a2f6ee16fbd36ffe17596d4c709dcdadbb3907de49be3052df3a"
+      sha256 "0179e46906380e9b2895461a5c0578ff4550cac6d26d3123a18f2cbc5552ae86"
     end
     on_arm do
-      url "https://github.com/turn-radius/flightplan/releases/download/v0.2.1/flightplan_linux_arm64.tar.gz",
+      url "https://github.com/turn-radius/flightplan/releases/download/v0.2.2/flightplan_linux_arm64.tar.gz",
           using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "cac53debc9e887df5e260197a45353da45115a7a3a16dc0d734273969ce60530"
+      sha256 "225eb366488a0f0e852fec57d1c7885d23276b54a60b39e19d071b49d09d9486"
     end
   end
 
